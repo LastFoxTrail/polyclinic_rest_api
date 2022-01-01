9fb08d944f3894a62901c5b4976df8a63f944e02
LastFoxTrail <liza.lis.2505@mail.ru>
2022-01-02 02:04:39 +0300

    added test directory

---
+++ src/test/java/com/lis/projects/PolyclinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lis.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyclinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
